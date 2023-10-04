3fa45f64a99d0b38fcaeb66a4c05854b7bcd43f7
João Victor da Silva <jv782063@gmail.com>
2023-10-04 00:07:58 -0300

    create addCategory usecase

---
+++ src/domain/usecases/product/category/addCategory.ts
@@ -0,0 +1,12 @@
+import { CategoryEntity } from '@/domain/entities/product';
+
+export interface AddCategory {
+  add: (params: AddCategory.Params) => Promise<AddCategory.Result>;
+};
+
+export namespace AddCategory {
+  export type Params = CategoryEntity;
+
+  export type Result = void;
+};
+
--- src/domain/usecases/product/category/index.ts
+++ src/domain/usecases/product/category/index.ts
@@ -1,3 +1,4 @@
+export * from './addCategory';
 export * from './loadCategories';
 export * from './loadCategoryById';
 
